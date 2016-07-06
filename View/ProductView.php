b51a7ea410c7dd58921454f317d6048454c13ba0
Kortium <ieslondor@gmail.com>
2016-07-06 22:09:34 +0300

    Продукт вью

--- View/ProductView.php
+++ View/ProductView.php
@@ -23,12 +23,40 @@
 
 			<!-- Профиль -->
 			<section class="person">         
-			    <div class="image left">
-			    	<img id="avatarImage" src="http://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c04386283.png" />
-			    </div>
+		    	<img id="avatarImage" class="image left" src="http://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c04386283.png" height="200" weight="200"/>
 			    <div class="inner">
 			    	<section>
-			    		ЙОХОХОХОЛ
+				    	<?=$this->parametersWithTranslate["CountTickets"].": "?><?=$this->result["CountTickets"]?> шт. <br>
+
+				    	<?=$this->parametersWithTranslate["Speed"].": "?><?=$this->result["Speed"]?> мм/сек<br>
+
+				    	<?=$this->parametersWithTranslate["DiamSleeveTicket"].": "?><?=$this->result["DiamSleeveTicket"]?> ХУЙ Знаект <br>
+
+				    	<?=$this->parametersWithTranslate["MaxDiamRollTicket"].": "?><?=$this->result["MaxDiamRollTicket"]?> Но молчит<br>
+
+				    	<?=$this->parametersWithTranslate["DiamSleeveRibbon"].": "?><?=$this->result["DiamSleeveRibbon"]?> Bitch<br>
+
+				    	<?=$this->parametersWithTranslate["MaxWoundRibbon"].": "?><?=$this->result["MaxWoundRibbon"]?> Это вообще ересь<br>
+
+				    	<?=$this->parametersWithTranslate["MaxPrintingWidth"].": "?><?=$this->result["MaxPrintingWidth"]?> Ширина<br>
+
+				    	<?=$this->parametersWithTranslate["PrinterType"].": "?><?=$this->parametersWithTranslate[$this->result["PrinterType"]]?> <br>
+
+				    	<?=$this->parametersWithTranslate["DPI"].": "?><?=$this->result["DPI"]?> <br>
+
+				    	<?=$this->parametersWithTranslate["PrintingType"].": "?><?=$this->parametersWithTranslate[$this->result["PrintingType"]]?> <br>
+
+				    	<?=$this->parametersWithTranslate["ModelName"].": "?><?=$this->result["ModelName"]?> <br>
+
+				    	<?=$this->parametersWithTranslate["VendorName"].": "?><?=$this->result["VendorName"]?> <br>
+
+				    	<?=$this->parametersWithTranslate["DisplayTypeName"].": "?><?=$this->result["DisplayTypeName"]?> <br>
+
+				    	<?=$this->parametersWithTranslate["UseKnife"].": "?><?=$this->parametersWithTranslate[$this->result["UseKnife"]]?> <br>
+				    	<?=$this->parametersWithTranslate["UseSeparator"].": "?><?=$this->parametersWithTranslate[$this->result["UseSeparator"]]?> <br>
+				    	<?=$this->parametersWithTranslate["UseEthernet"].": "?><?=$this->parametersWithTranslate[$this->result["UseEthernet"]]?> <br>
+				    	<?=$this->parametersWithTranslate["UseWinder"].": "?><?=$this->parametersWithTranslate[$this->result["UseWinder"]]?> <br>
+				    	<?=$this->parametersWithTranslate["Winding"].": "?><?=$this->parametersWithTranslate[$this->result["Winding"]]?> <br>
 					</section>
 				</div>
 			</section>
--- product.php
+++ product.php
@@ -2,8 +2,12 @@
 	include_once('controller.php');
 
 	class ProductController extends Controller {
+		public $result;
+		public $parametersWithTranslate;
+
 		function __construct()
 		{
+
 			parent::__construct();
 
 			$parametersWithTypes = array(
@@ -29,13 +33,55 @@
 				"DisplayTypeID"=>'i'
 			);
 
-			
+			$this->parametersWithTranslate = array(
+				"RowID"=>'АйДи Приблуды',
+				"CountTickets"=>'Кол-во этикеток',
+				"Speed"=>'Скорость печати',
+				"UseKnife"=>'Наличие ножа',
+				"UseSeparator"=>'Наличие отделителя',
+				"UseEthernet"=>'Наличие Ethernet',
+				"Price"=>'Цена',
+				"DiamSleeveTicket"=>'Диаметр втулки этикетки',
+				"MaxDiamRollTicket"=>'Максимальный диаметр рулона этикеток',
+				"DiamSleeveRibbon"=>'Диаметр втулки риббона',
+				"MaxWoundRibbon"=>'Максимальная намотка риббона',
+				"MaxPrintingWidth"=>'Максимальная ширина печати',
+				"UseWinder"=>'Наличие смотчика',
+				"SKU"=>'Парт номер',
+				"PrinterType"=>'Тип принтера',
+				"DPI"=>'DPI',
+				"Winding"=>'Намотки',
+				"PrintingType"=>'Тип печати',
+				"ModelName"=>'Модель',
+				"VendorName"=>'Производитель',
+				"DisplayTypeName"=>'Дисплей',
+
+				"Direct Thermal"=>'DT',
+				"Thermal Transfer"=>'TT',
+				"DT & TT"=>'DT&TT',
+				"Dot Matrix"=>'Матричный',
+				"Mobile"=>'Мобильный',
+				"Desktop"=>'Настольный',
+				"Industrial"=>'Промышленный',
+				"1"=>"Есть",
+				"0"=>"Нет",
+				1=>"Есть",
+				0=>"Нет",
+
+				"in"=>"Внутренняя",
+				"out"=>"Наружная",
+				"all"=>"Внутренняя и наружная",
+				"no winding"=>"Нет"
+			);
+			$id=$this->parseGETParameter('id');
+			$id=10;
+			$this->backend->addWhereParameter('RowID', $id, 'i');
+			$this->result=$this->backend->getData()[0];
 			
 			$this->showPage();
 		}
 
 		private function showPage() {
-			print_r($this->backend->getWindings());
 	 		$this->setPageTitle('Модель продукта');
 	 		include_once("View/ProductView.php");
 		}
