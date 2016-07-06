8836663badb9d9cc0430076a85933f84e2babeb8
Alex Kuzovkov <alexeykuzovkov@yandex.ru>
2016-07-06 15:08:45 +0300

    jquery

--- View.php
+++ View.php
@@ -3,24 +3,42 @@
 	<title>Trainington</title>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link rel="stylesheet" href="js/jquery-ui.min.css" />
+	<link rel="stylesheet" href="css/jquery-ui.min.css" />
+	<link rel="stylesheet" href="css/main.css" />
 </head>
 <body>
-	<form>
-	  <div id="radio">
-	    <input type="radio" id="radio1" name="radio"><label for="radio1">Вариант 1</label>
-	    <input type="radio" id="radio2" name="radio" checked="checked"><label for="radio2">Вариант 2</label>
-	    <input type="radio" id="radio3" name="radio"><label for="radio3">Вариант 3</label>
-	  </div>
-	</form>
+	<div id="recepieView">
+		<form>
+		  <div id="radio">
+		    <input type="radio" id="radio1" name="radio"><label for="radio1">Вариант 1</label>
+		    <input type="radio" id="radio2" name="radio" checked="checked"><label for="radio2">Вариант 2</label>
+		    <input type="radio" id="radio3" name="radio"><label for="radio3">Вариант 3</label>
+		  </div>
+		</form>
+	</div>
+	<div id="recepieDetailsView">
+		<ul>
+			<li>Вариант</li>
+			<li>Вариант</li>
+			<li>Вариант</li>
+			<li>Вариант</li>
+		</ul>
+	</div>
+
+	<div id="viewAllView">
+		<a href="index.php?filter=true">Поиск по параметрам</a>
+	</div>
 
 	<script src="js/jquery.js"></script>
 	<script src="js/jquery-ui.min.js"></script>
 
 	<script type="text/javascript">
 		$(function() {
-		    $( "#radio" ).buttonset();
-		  });
+			$( "#radio" ).buttonset();
+		});
+		$(function() {
+			$( "#recepieDetailsView ul" ).menu();
+		});
 	</script>
 </body>
 </html>
